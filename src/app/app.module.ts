e4ce9c33b19fc0d76437f411219d81c73d755b35
mattbryan9 <mbryan@yoobic.com>
2017-04-21 17:31:49 +0100

    use lazy loading for list page, navigate only using string references to IonicPages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
-    MyApp,
-    ListPage
+    MyApp
   ],
   imports: [
     BrowserModule,
@@ -19,8 +17,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    ListPage
+    MyApp
   ],
   providers: [
     StatusBar,
