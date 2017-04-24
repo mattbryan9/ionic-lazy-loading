2ab77c9bb9ac1822b75e544e19cabb0c5ffe8992
mattbryan9 <mbryan@yoobic.com>
2017-04-24 09:10:18 +0100

    added planetService for fun or whatever

--- src/pages/list/list.module.ts
+++ src/pages/list/list.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ListPage} from './list';
 import { IonicPageModule } from 'ionic-angular';
+import { PlanetService } from '../../services/planet/planet.service';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [ListPage],
-  imports: [IonicPageModule.forChild(ListPage)],
+  imports: [IonicPageModule.forChild(ListPage), HttpModule],
+  providers: [PlanetService]
 })
 export class ListPageModule { }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,38 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams, IonicPage } from 'ionic-angular';
+
+import { PlanetService } from '../../services/planet/planet.service';
 
 @IonicPage()
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
-export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+export class ListPage implements OnInit {
+  planet: any;
+  planets: Array<any>;
+  selectedPlanet: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public planetService: PlanetService) {
+    this.planet = navParams.get('planet');
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
+
+  itemTapped(event, planet) {
     this.navCtrl.push(ListPage, {
-      item: item
+      planet: planet
+    });
+  }
+
+  ngOnInit() {
+    this.planetService.getPlanets().subscribe(planets => {
+      this.planets = planets.results;
+      console.log(this.planets);
     });
   }
 }
